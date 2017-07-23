29d69424179b23635989905a4ea14c3e81623a63
Dave Sims <davsims@gmail.com>
2017-07-23 17:05:02 -0500

    Removing unique ip for now

--- kuzu_stats.rb
+++ kuzu_stats.rb
@@ -9,11 +9,10 @@ database = "kuzu_stats"
 @influxdb = InfluxDB::Client.new database, host: "localhost", username: username, password: password
 
 while true do
-  clients = HTTParty.get("http://kuzu.stream:8000/admin/listclients?mount=/kuzu.mp3", :basic_auth => auth)
-  ip_count = clients["icestats"]["source"]["listener"].size
-  unique_ip_count = { values: { value: ip_count } }
-  ap unique_ip_count
-  @influxdb.write_point("unique_ips", unique_ip_count)
+  #clients = HTTParty.get("http://kuzu.stream:8000/admin/listclients?mount=/kuzu.mp3", :basic_auth => auth)
+  #ip_count = clients["icestats"]["source"]["listener"].size
+  #unique_ip_count = { values: { value: ip_count } }
+  #@influxdb.write_point("unique_ips", unique_ip_count)
 
   stats = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
   listeners = stats["icestats"]["listeners"]
