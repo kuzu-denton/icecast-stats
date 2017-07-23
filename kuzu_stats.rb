202ec8dc57e574e6dc19fd8e2fdcc14b908b98b4
Dave Sims <davsims@gmail.com>
2017-07-23 14:26:18 -0500

    ENV all the things

--- kuzu_stats.rb
+++ kuzu_stats.rb
@@ -1,5 +1,5 @@
 require "httparty"
 require "ap"
-auth = {:username => "admin", :password => ENV["KUZUADMIN"]}
+auth = {:username => ENV["KUZUNAME"], :password => ENV["KUZUPW"]}
 response = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
 ap response
