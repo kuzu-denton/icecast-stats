6cc6b7198e62b50a4b1057ac52ae8357039726c1
Dave Sims <davsims@gmail.com>
2017-07-23 17:01:34 -0500

    Add unique ip count

--- kuzu_stats.rb
+++ kuzu_stats.rb
@@ -6,16 +6,20 @@ auth = {:username => ENV["KUZUNAME"], :password => ENV["KUZUPW"]}
 username = "admin"
 password = "admin"
 database = "kuzu_stats"
-@table     = "current_listeners"
 @influxdb = InfluxDB::Client.new database, host: "localhost", username: username, password: password
 
 while true do
-  response = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
-  listeners = response["icestats"]["listeners"]
-  ap listeners
-  data = {
+  clients = HTTParty.get("http://kuzu.stream:8000/admin/listclients?mount=/kuzu.mp3", :basic_auth => auth)
+  ip_count = clients["icestats"]["source"]["listener"].size
+  unique_ip_count = { values: { value: ip_count } }
+  ap unique_ip_count
+  @influxdb.write_point("unique_ips", unique_ip_count)
+
+  stats = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
+  listeners = stats["icestats"]["listeners"]
+  current_listeners_data = {
     values: { value: listeners.to_i }
   }
-  @influxdb.write_point(@table, data)
+  @influxdb.write_point("current_listeners", current_listeners_data)
   sleep 3
 end
