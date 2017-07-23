f605cf26a1d6b47dfb3d8e267502980681a672f9
Dave Sims <davsims@gmail.com>
2017-07-23 14:47:37 -0500

    Update influx every second

--- kuzu_stats.rb
+++ kuzu_stats.rb
@@ -1,5 +1,21 @@
 require "httparty"
+require "influxdb"
 require "ap"
+
 auth = {:username => ENV["KUZUNAME"], :password => ENV["KUZUPW"]}
-response = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
-ap response
+username = "admin"
+password = "admin"
+database = "kuzu_stats"
+@table     = "current_listeners"
+@influxdb = InfluxDB::Client.new database, host: "localhost", username: username, password: password
+
+while true do
+  response = HTTParty.get("http://kuzu.stream:8000/admin/stats", :basic_auth => auth)
+  listeners = response["icestats"]["listeners"]
+  ap listeners
+  data = {
+    values: { value: listeners }
+  }
+  @influxdb.write_point(@table, data)
+  sleep 1
+end
