e488ffec805db7cb3a7b9697667af8f1018fda81
Dave Sims <davsims@gmail.com>
2017-07-30 15:02:15 -0500

    Make aggregator a singleton

--- lib/icecast/stats/aggregator.rb
+++ lib/icecast/stats/aggregator.rb
@@ -1,6 +1,16 @@
+require "forwardable"
+require "singleton"
+
 module IceCast::Stats
   class Aggregator
-    def self.record_current_listeners
+    include Singleton
+
+    class << self
+      extend Forwardable
+      def_delegators :instance, :record_current_listeners
+    end
+
+    def record_current_listeners
       listeners = IceCast::Admin.get_current_listeners
       InfluxDB::Connection.write_current_listeners(listeners)
     end
